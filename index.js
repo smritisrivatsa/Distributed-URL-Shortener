5d071e1567d209984ab4adc6d59aa4696e018a64
smritisrivatsa <smrits3@uw.edu>
2026-05-18 23:18:18 -0700

    website

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+<<<<<<< Updated upstream
 import base62 from "base62/lib/ascii.js";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
@@ -61,3 +62,24 @@ async function createShortUrl(longUrl) {
 }
 
 createShortUrl('https://systemdsdfsesignschool.io/problems/url-shortener/solution');
+=======
+import { createShortUrl } from "./src/urlService.js";
+import express from 'express';
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/shorten', async (req, res) => {
+  const { longUrl } = req.body;
+  const shortCode = await createShortUrl(longUrl);
+  res.json({ shortCode });
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
+
+// const shortCode = await createShortUrl('https://universityofwashingtonhfs8.humanity.com/app/staff/detail/8759105/');
+// console.log("Short code:", shortCode);
+>>>>>>> Stashed changes
