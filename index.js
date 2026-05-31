4c38b8a084679e17ea9333b9fe499960ec27419f
akomp23 <aditya23.kompella@gmail.com>
2026-05-30 23:06:12 -0700

    setup redis and got redirection caching working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import { createShortUrl, getOriginalUrl } from "./src/urlService.js";
+import { getCachedUrl, insertUrl } from './src/cache.js';
 
 const app = express();
 app.use(express.static('public'));
@@ -12,8 +13,36 @@ app.post('/shorten', async (req, res) => {
   res.json({ shortCode });
 });
 
+/*
+ * Check if cache has this shortCode
+ * If it does, redirect to the corresponding longUrl
+ * If it doesn't,
+  * Check if dynamo has this shortCode
+  * If it does, 
+    * Update cache to have this shortCode, longUrl pair
+    * Redirect to the corresponding longUrl
+  * If it doesn't, throw some error "short code not found"
+*/
 app.get("/:shortCode", async (req, res) => {
-  const longUrl = await getOriginalUrl(req.params.shortCode);
+  const longUrlRedis = await getCachedUrl(req.params.shortCode);
+  if (longUrlRedis) {
+    res.redirect(302, longUrlRedis);
+  } else {
+    const longUrlDynamo = await getOriginalUrl(req.params.shortCode);
+    if (longUrlDynamo) {
+      insertUrl(req.params.shortCode, longUrlDynamo);
+      res.redirect(302, longUrlDynamo);
+    } else {
+      return res.status(404).send("Not found");
+    }
+  }
+
+
+
+
+
+  
+  
 
   if (!longUrl) return res.status(404).send("Not found");
 
