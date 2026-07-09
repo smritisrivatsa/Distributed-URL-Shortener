d432783efba741e68236549651b141aea8cf67df
akomp23 <aditya23.kompella@gmail.com>
2026-07-09 12:48:53 -0400

    initial test

--- write_service/index.js
+++ write_service/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'https://frontend-9hd329rfo-url-shortener.vercel.app']
 }));
 
 app.post('/shorten', async (req, res) => {
